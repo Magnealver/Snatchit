71d3806113556343e310d1cf5f2fd42048889b6b
Magne Alver <Magne.alver@gmail.com>
2018-07-29 10:24:04 +0200

    start memes

---
+++ tradingpostmemes/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Magne\source\repos\Snatchit\tradingpostmemes\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
