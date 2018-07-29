c8f3c4c0e1348df200358f3aab11b40601e2a9c0
Magne Alver <Magne.alver@gmail.com>
2018-07-29 10:38:56 +0200

    memes

--- tradingpostmemes/app/views.py
+++ tradingpostmemes/app/views.py
@@ -6,16 +6,26 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
+import json
+import requests
 
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
+    api_url_base = 'https://api.guildwars2.com/v2/'
+    response  = requests.get('https://api.guildwars2.com/v2/commerce/listings/19684')
+    listingdata = response.json()
+
+
+
     return render(
         request,
         'app/index.html',
         {
             'title':'Home Page',
             'year':datetime.now().year,
+            'id':listingdata['id'],
+            'lowest sell':listingdata['sells'],
         }
     )
 
